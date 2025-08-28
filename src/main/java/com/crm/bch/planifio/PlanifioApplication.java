4fd874166c557f0f44b4c7a54e40becd008cd398
bastien <bchevriaut@gmail.com>
2025-08-28 19:16:22 +0200

    first commit

---
+++ src/main/java/com/crm/bch/planifio/PlanifioApplication.java
@@ -0,0 +1,13 @@
+package com.crm.bch.planifio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanifioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanifioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crm/bch/planifio/PlanifioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crm.bch.planifio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanifioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
